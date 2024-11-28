eabcde787f6c5e6e06aeb2ea41b9e9d2c2bd1510
ARIKBOGA <barikboga42@gmail.com>
2024-11-28 17:38:11 +0300

    additional exercises have been done

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         int age = 21;
 
-        String s1 = age >= 18 ? "Adult" :"Minor";
+        String s1 = age >= 18 ? "Adult" : "Minor";
 
         System.out.println(s1);
 
+        System.out.println(1 == 1 ? "True" : "False");
 
     }
 }
